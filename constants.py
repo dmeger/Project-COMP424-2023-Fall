84997e6dba9f60061acd453c6afe602758fa7881
David Meger <david.meger@gmail.com>
2023-11-03 20:19:07 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # Constants used throughout the game
-MIN_BOARD_SIZE = 5
-MAX_BOARD_SIZE = 10
+MIN_BOARD_SIZE = 6
+MAX_BOARD_SIZE = 12
 AGENT_NOT_FOUND_MSG = (
     "Check if you have used the decorator @register_agent to register your agent!"
 )
