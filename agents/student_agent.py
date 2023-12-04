5cbffdae197d56c15400c1ca6fe85bcd9608a1be
Justin-Randisi <justin.randisi@mail.mcgill.ca>
2023-12-04 17:54:51 -0500

    alpha-beta

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -47,6 +47,86 @@ class StudentAgent(Agent):
         time_taken = time.time() - start_time
         
         print("My AI's turn took ", time_taken, "seconds.")
+        # Translate the observation into your game state representation
+
+        # Call the alpha-beta search to get the best move
+        best_move = self.alpha_beta_search(obs)
+        return best_move
 
         # dummy return
-        return my_pos, self.dir_map["u"]
+        # return my_pos, self.dir_map["u"]
+ 
+    def alpha_beta_search(self, state):
+        # Implement the alpha-beta search algorithm
+        # Initialize alpha and beta values
+        alpha = float('-inf')
+        beta = float('inf')
+
+        # Initially call the max_value function
+        best_score, best_move = self.max_value(state, alpha, beta, 0)
+        return best_move
+
+    def max_value(self, state, alpha, beta, depth):
+        # Check for terminal state or maximum depth
+        if self.is_terminal(state) or depth == MAX_DEPTH:
+            return self.evaluate(state), None
+
+        v = float('-inf')
+        best_move = None
+        for move in self.get_possible_moves(state):
+            # Apply the move
+            new_state = self.apply_move(state, move)
+            # Call min_value
+            min_v, _ = self.min_value(new_state, alpha, beta, depth + 1)
+            if min_v > v:
+                v = min_v
+                best_move = move
+            if v >= beta:
+                return v, best_move
+            alpha = max(alpha, v)
+        return v, best_move
+
+    def min_value(self, state, alpha, beta, depth):
+        # Check for terminal state or maximum depth
+        if self.is_terminal(state) or depth == MAX_DEPTH:
+            return self.evaluate(state), None
+
+        v = float('inf')
+        best_move = None
+        for move in self.get_possible_moves(state):
+            # Apply the move
+            new_state = self.apply_move(state, move)
+            # Call max_value
+            max_v, _ = self.max_value(new_state, alpha, beta, depth + 1)
+            if max_v < v:
+                v = max_v
+                best_move = move
+            if v <= alpha:
+                return v, best_move
+            beta = min(beta, v)
+        return v, best_move
+    
+    def evaluate(self, state):
+        # Evaluate the game state and return a heuristic score
+        # ...
+        return 0
+
+    def is_terminal(self, state):
+        # Determine if the game state is a terminal state
+        # ...
+        return False
+
+    def get_possible_moves(self, state):
+        # Generate and return all possible legal moves from the current state
+        # ...
+        return 0
+
+    def apply_move(self, state, move):
+        # Apply a move to the state and return the new state
+        # ...
+        return 0
+
+    def parse_observation(self, obs):
+        # Translate the observation into a game state representation
+        # ...
+        return 0
