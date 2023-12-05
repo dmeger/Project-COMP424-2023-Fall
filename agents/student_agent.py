553608113e0c63c6f74f06eead79f019e64e4396
Justin-Randisi <justin.randisi@mail.mcgill.ca>
2023-12-04 23:32:56 -0500

    progress

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -49,35 +49,31 @@ class StudentAgent(Agent):
         print("My AI's turn took ", time_taken, "seconds.")
         # Translate the observation into your game state representation
 
-        # Call the alpha-beta search to get the best move
-        best_move = self.alpha_beta_search(obs)
-        return best_move
-
-        # dummy return
-        # return my_pos, self.dir_map["u"]
- 
-    def alpha_beta_search(self, state):
         # Implement the alpha-beta search algorithm
         # Initialize alpha and beta values
         alpha = float('-inf')
         beta = float('inf')
 
         # Initially call the max_value function
-        best_score, best_move = self.max_value(state, alpha, beta, 0)
+        best_score, best_move = self.max_value(chess_board, my_pos, adv_pos, max_step, alpha, beta, 0)
         return best_move
 
-    def max_value(self, state, alpha, beta, depth):
-        # Check for terminal state or maximum depth
-        if self.is_terminal(state) or depth == MAX_DEPTH:
-            return self.evaluate(state), None
+        # dummy return
+        # return my_pos, self.dir_map["u"]
 
+    def max_value(self, chess_board, my_pos, adv_pos, max_step, alpha, beta, depth):
+        # Check for terminal state or maximum depth
+        if self.is_terminal(chess_board, my_pos):
+            return self.evaluate(chess_board), None
+        
         v = float('-inf')
         best_move = None
-        for move in self.get_possible_moves(state):
+        allowed_dirs = self.get_possible_moves(chess_board, my_pos, adv_pos, max_step)
+        for move in allowed_dirs:
             # Apply the move
-            new_state = self.apply_move(state, move)
+            new_board = self.apply_move(chess_board, my_pos, move)
             # Call min_value
-            min_v, _ = self.min_value(new_state, alpha, beta, depth + 1)
+            min_v, _ = self.min_value(new_board, alpha, beta, depth + 1)
             if min_v > v:
                 v = min_v
                 best_move = move
@@ -86,18 +82,19 @@ class StudentAgent(Agent):
             alpha = max(alpha, v)
         return v, best_move
 
-    def min_value(self, state, alpha, beta, depth):
+    def min_value(self, chess_board, my_pos, adv_pos, max_step, alpha, beta, depth):
         # Check for terminal state or maximum depth
-        if self.is_terminal(state) or depth == MAX_DEPTH:
-            return self.evaluate(state), None
+        if self.is_terminal(chess_board, my_pos):
+            return self.evaluate(chess_board), None
 
         v = float('inf')
         best_move = None
-        for move in self.get_possible_moves(state):
+        allowed_dirs = self.get_possible_moves(chess_board, my_pos, adv_pos, max_step)
+        for move in allowed_dirs:
             # Apply the move
-            new_state = self.apply_move(state, move)
+            new_board = self.apply_move(chess_board, move)
             # Call max_value
-            max_v, _ = self.max_value(new_state, alpha, beta, depth + 1)
+            max_v, _ = self.max_value(new_board, alpha, beta, depth + 1)
             if max_v < v:
                 v = max_v
                 best_move = move
@@ -111,22 +108,36 @@ class StudentAgent(Agent):
         # ...
         return 0
 
-    def is_terminal(self, state):
-        # Determine if the game state is a terminal state
-        # ...
-        return False
+    def is_terminal(self, chess_board, my_pos):
+        #code taken from random_agent.py
+        
+        r, c = my_pos
+        # Possibilities, any direction such that chess_board is False
+        allowed_barriers=[i for i in range(0,4) if not chess_board[r,c,i]]
+        # Sanity check, no way to be fully enclosed in a square, else game already ended
+        return not(len(allowed_barriers)>=1) 
 
-    def get_possible_moves(self, state):
+    def get_possible_moves(self, chess_board, my_pos, adv_pos, max_step):
         # Generate and return all possible legal moves from the current state
-        # ...
-        return 0
+        #CODE USED FROM random_agent.py
+        
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        allowed_moves = []
+        for step in range(max_step):
+            r, c = my_pos
+            
+            allowed_dirs = [ d                                
+                for d in range(0,4)                           # 4 moves possible
+                if not chess_board[r,c,d] and                 # chess_board True means wall
+                not adv_pos == (r+moves[d][0],c+moves[d][1])] # cannot move through Adversary
+
+            if len(allowed_dirs)==0:
+                # If no possible move, we must be enclosed by our Adversary
+                break
+
+        return allowed_dirs
 
     def apply_move(self, state, move):
         # Apply a move to the state and return the new state
-        # ...
-        return 0
-
-    def parse_observation(self, obs):
-        # Translate the observation into a game state representation
         # ...
         return 0
