4f38871276b6d6fd8f2654fd5c09809a864c416d
Justin-Randisi <justin.randisi@mail.mcgill.ca>
2023-12-06 18:21:19 -0500

    working robot

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -6,7 +6,6 @@ import numpy as np
 from copy import deepcopy
 import time
 
-moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
 @register_agent("student_agent")
 class StudentAgent(Agent):
@@ -14,6 +13,7 @@ class StudentAgent(Agent):
     def __init__(self):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
+        self.autoplay = True
         self.dir_map = {
             "u": 0,
             "r": 1,
@@ -22,104 +22,157 @@ class StudentAgent(Agent):
         }
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
+        max_depth = 5
+        start_time = time.time()
 
-        #start_time = time.time()
-        #time_taken = time.time() - start_time
-        #print("My AI's turn took ", time_taken, "seconds.")
-
-        # include directions
-        m_r, m_c = my_pos
-        my_pos = (m_r, m_c, 0)
-
+        alpha = float('-inf')
+        beta = float('-inf')
         v = float('-inf')
-        list_best_moves = []
+        best_moves = []
 
-        for moves in self.allowed_moves(chess_board, (m_r, m_c), max_step, adv_pos):
+        for move in self.allowed_moves(chess_board, my_pos, max_step, adv_pos):
             # Apply the move
-            chess_board = self.apply_move(chess_board, moves)
+            chess_board = self.apply_move(chess_board, move)
             
-            if self.is_terminal(chess_board, moves):
-                value = self.evaluate(chess_board, moves, adv_pos, max_step)
+            # call min_value
+            if max_depth == 0 or self.is_terminal(chess_board, move):
+                value = self.evaluate(chess_board, move, adv_pos, max_step)
             else:
-                # call min_value
-                value = self.min_value(chess_board, moves, adv_pos, max_step)
+                value = self.min_value(chess_board, move, adv_pos, max_step, alpha, beta, max_depth - 1, start_time)
             if value > v:
                 v = value
-                list_best_moves.clear()
-                list_best_moves.append(moves)
+                best_moves.clear()
+                best_moves.append(move)
+            if v >= value:
+                break
+            alpha = max(alpha, v)
+            
+            # Make sure each move takes less than 2 seconds
+            time_taken = time.time() - start_time
+            if time_taken > 1.85:
+                break
 
-            if value == v:
-                list_best_moves.append(moves)
+        i = 0
+        if len(best_moves) > 1:
+            length_list = len(best_moves)
+            i = np.random.randint(0, length_list)
 
-        a = 0
-        if len(list_best_moves) > 1:
-            length_list = len(list_best_moves)
-            a = np.random.randint(0, length_list)
-
-        r, c, d = list_best_moves[a]
+        r, c, d = best_moves[i]
         return (r, c), d
-    
-    def max_value(self, chess_board, original_position, adv_pos, max_step): 
+
+    def max_value(self, chess_board, move, adv_pos, max_step, alpha, beta, max_depth, start_time): 
         v = float('-inf')
-        m_r, m_c, m_d = original_position
+        # Make sure each move takes less than 2 seconds
+        if max_depth == 0 or self.is_terminal(chess_board, move):
+            return self.evaluate(chess_board, move, adv_pos, max_step)
+        
+        m_r, m_c, m_d = move
         for moves in self.allowed_moves(chess_board, (m_r, m_c), max_step, adv_pos):
             chess_board = self.apply_move(chess_board, moves)
-            score = self.evaluate(chess_board, moves, adv_pos, max_step)
-            if score > v:
-                v = score
+            # score = self.evaluate(chess_board, moves, adv_pos, max_step)
+            min_v = self.min_value(chess_board, moves, adv_pos, max_step, alpha, beta, max_depth - 1, start_time)
+            if min_v > v:
+                v = min_v
+            if v >= beta: 
+                return v
+            alpha = max(alpha, v)
+            # time check
+            time_taken = time.time() - start_time
+            if time_taken > 1.85:
+                break
         return v
 
-    def min_value(self, chess_board, original_position, adv_pos, max_step):
+    def min_value(self, chess_board, move, adv_pos, max_step, alpha, beta, max_depth, start_time):
         v = float('inf')
-        m_r, m_c, m_d = original_position
+        if max_depth == 0 or self.is_terminal(chess_board, move):
+            return self.evaluate(chess_board, move, adv_pos, max_step)
+        
+        m_r, m_c, m_d = move
         for moves in self.allowed_moves(chess_board, adv_pos, max_step, (m_r, m_c)):
             chess_board = self.apply_move(chess_board, moves)
-            score = self.evaluate(chess_board, original_position, adv_pos, max_step)
-            if score < v:
-                v = score
-
+            # score = self.evaluate(chess_board, move, adv_pos, max_step)
+            max_v = self.max_value(chess_board, moves, adv_pos, max_step, alpha, beta, max_depth - 1, start_time)
+            if max_v < v:
+                v = max_v
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+            # time check
+            time_taken = time.time() - start_time
+            if time_taken > 1.85:
+                break
         return v
 
-    def check_boundary(self, chess_board, pos):
-        r, c = pos
-        si = int(chess_board[0].size/4)
-        si = si - 1
-        return 0 <= r < si and 0 <= c < si
+    def check_boundary(self, chess_board, dest_pos):
+        r, c = dest_pos
+        # Wall coordinates
+        wall = int(chess_board[0].size/4) - 1
+        return 0 <= r < wall and 0 <= c < wall
 
-    def check_valid_step(self, chess_board, start_pos, end_pos, barrier_dir, max_step1, adv_pos):
-        # Endpoint already has barrier or is boarder
+    def check_valid_step(self, chess_board, start_pos, end_pos, d, max_step, adv_pos):
+        # CODE TAKEN FROM WORLD.py
+
         chess_board = deepcopy(chess_board)
-
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        # Endpoint already has barrier or is border
         r, c = end_pos
 
         if not self.check_boundary(chess_board, end_pos):
             return False
-
-        if chess_board[r, c, barrier_dir]:
+        if chess_board[r, c, d]:
             return False
-        
-        return True
+        if np.array_equal(start_pos, end_pos):
+            return True
+
+        # BFS
+        state_queue = [(start_pos, 0)]
+        visited = {tuple(start_pos)}
+        is_reached = False
+        while state_queue and not is_reached:
+            cur_pos, cur_step = state_queue.pop(0)
+            r, c = cur_pos
+            if cur_step == max_step:
+                break
+            for dir, move in enumerate(moves):
+                if chess_board[r, c, dir]:
+                    continue
+
+                r1, c1 = move
+                r2, c2 = cur_pos
+
+                next_pos = (r2 + r1, c2 + c1)
+
+                if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
+                    continue
+                if np.array_equal(next_pos, end_pos):
+                    is_reached = True
+                    break
+
+                visited.add(tuple(next_pos))
+                state_queue.append((next_pos, cur_step + 1))
+
+        return is_reached
 
     def allowed_moves(self, chess_board, my_pos, max_step, adv_pos):
 
         move_list = []      # Initialize a list of moves
         r, c = my_pos       # separate my_pos into x and y coordinates
-        r1 = r - max_step   
-        c1 = c              
+        r_1 = r - max_step   
+        c_1 = c              
 
         # for each direction move and wall for max_step
         for d in range(4):
             for x in range(max_step + 1):
                 for y in range(-x, x + 1, 1):
-                    if self.check_valid_step(chess_board, my_pos, (r1 + x, c1 + y), d, max_step, adv_pos):
-                        move_list.append((r1 + x, c1 + y, d))
+                    if self.check_valid_step(chess_board, my_pos, (r_1 + x, c_1 + y), d, max_step, adv_pos):
+                        move_list.append((r_1 + x, c_1 + y, d))
 
-            r2 = r + max_step
-            c2 = c
+            r_2 = r + max_step
+            c_2 = c
             for x in range(max_step - 1, -1, -1):
                 for y in range(-x, x + 1, 1):
-                    if self.check_valid_step(chess_board, my_pos, (r2-x,c2-y), d, max_step, adv_pos):
-                        move_list.append((r2 - x, c2 - y, d))
+                    if self.check_valid_step(chess_board, my_pos, (r_2 - x, c_2 - y), d, max_step, adv_pos):
+                        move_list.append((r_2 - x, c_2 - y, d))
 
         return move_list
 
